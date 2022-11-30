2cc761a9a83848e1bf8b5800e7fa2b93f23e34cb
Saad575757 <smartysaad59@gmail.com>
2022-12-01 01:29:18 +0500

    Great

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,53 +3,7 @@ import { useEffect } from 'react';
 
 import "./App.css";
 
-// const FetchApi=()=>{
-  
-//   const [users, setUsers]=useState([]); //initial value will be an empty array.
 
-//   const getUsers=async()=>{
-//     const response= await  fetch("https://api.github.com/users");//this will be written promise.
-//     // const data=await response.json();
-//     // console.log(data);
-//     setUsers(await response.json());
-
-
-//   }
-
-//   useEffect(()=>{
-//     getUsers();
-//   },[]);
-//   return(
-//     <>
-//     <div>
-//       <h3>List of Github users</h3>
-//       <div className="card">
-//         {
-//           users.map((currelement)=>{
-//             return(
-//               <div>
-//                  <h5 className="name"> {currelement.login}</h5>
-//         <h6 className="passion">{currelement.id}</h6>
-//         <div className="inner-card">
-//           <p> {currelement.url}</p><p>500</p>
-//           <p>Articles</p><p>15</p>
-//           <p>Rating</p><p>8.9</p>
-//         </div>
-
-//               </div>
-//             )
-
-//           })
-//         }
-       
-//       </div>
-//     </div>
-//     </>
-
-//   );
-// }
-
-// export default FetchApi;
 
 const App=()=>{
 
