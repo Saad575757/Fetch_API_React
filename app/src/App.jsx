c6a6be958a7a51fee12e3086da3b607cae9de781
Saad575757 <smartysaad59@gmail.com>
2022-12-03 03:36:52 +0500

    done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,40 +1,40 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 
 import "./App.css";
 
 
 
-const App=()=>{
+const App = () => {
 
-  const [user, setUser]=useState([]);
-  const getUsers=async()=>{
-    const response= await fetch('https://jsonplaceholder.typicode.com/users');
+  const [user, setUser] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
     // const data= await response.json();
     // console.log(data);
     setUser(await response.json());
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getUsers()
-  },[]);
-  return(
+  }, []);
+  return (
     <>
-    <h2 className='heading'>List of Users</h2>
-    <div className='card'>
-      
-      {
-        user.map((currentelement)=>(
-          <div className='card-style'>
-        <p className='name'> {currentelement.name}</p>
-        <p className='id'>ID: {currentelement.id}</p>
-        <p className='username'>Username: {currentelement.username}</p>
-        <p className='email'>Email: {currentelement.email}</p>
-        <p className='phone'>Phone: {currentelement.phone}</p>
+      <h2 className='heading'>List of Users</h2>
+      <div className='card'>
+
+        {
+          user.map((currentelement) => (
+            <div className='card-style'>
+              <p className='name'> {currentelement.name}</p>
+              <p className='id'>ID: {currentelement.id}</p>
+              <p className='username'>Username: {currentelement.username}</p>
+              <p className='email'>Email: {currentelement.email}</p>
+              <p className='phone'>Phone: {currentelement.phone}</p>
+            </div>
+          ))
+        }
       </div>
-        ))
-      }
-    </div>
     </>
   )
 }
